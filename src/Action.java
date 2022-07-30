d9e9feb5308ac6d0f5c10ff5243f8f5cf96040cd
Babanejad - Maryam <babanejad-m@emdad.com>
2022-07-30 18:00:21 +0430

    Initial commit

---
+++ src/Action.java
@@ -0,0 +1,7 @@
+public class Action {
+    public static void main(String[] args) {
+        MyClass objectOne = new MyClass("mari",35,true);
+        objectOne.display();
+
+    }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    String name;
+    int age;
+    boolean trueOrFalse;
+    MyClass(String name,int age,boolean trueOrFalse){
+        this.name=name;
+        this.age =age;
+        this.trueOrFalse = trueOrFalse ;
+    }
+    public void display(){
+        System.out.println("name");
+        System.out.println(age);
+        System.out.println(trueOrFalse);
+    }
+}
